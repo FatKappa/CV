22bddd022a008eeefad3b79eb63436af3ce3e3d7
FatKappa <istratenicolaevlad02@gmail.com>
2021-01-22 00:09:38 +0200

    -

---
+++ public/script.js
@@ -0,0 +1,10 @@
+
+function vizibilitate() {
+
+    const schimb = document.getElementById('ham');
+
+    if(schimb.style.display == 'flex') schimb.style.display = 'none';
+                                   else{
+                                       schimb.style.display = 'flex';
+                                    }
+}
