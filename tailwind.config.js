9e5f50395f425286b3ee61eb19d7d2b8a98d6219
FatKappa <59336336+FatKappa@users.noreply.github.com>
2024-03-15 21:36:41 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Inter':['"Inter"','serif']
+      },
+      spacing:{
+        '880':'55rem',
+        '656':'41rem',
+      },
+      colors:{
+        'cardbg':'rgb(23, 23, 23)'
+      },
+    },
+  },
+  plugins: [],
+}
+
